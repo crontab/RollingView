4b3182d6db3c17bb5f81fa05c0fce8c2c14193c7
Hovik Melikyan <hovik.melikyan@gmail.com>
2019-06-05 22:33:51 +0200

    Initial commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Hovik Melikyan on 05/06/2019.
+//  Copyright © 2019 Hovik Melikyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+	@IBOutlet weak var scrollView: UIScrollView!
+
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+}
+
